6522c9e7d66fa794856c6b0c9aae1e81386aca23
FairySaikiNoKysyo <michaelevgeniymyers@gmail.com>
2024-12-27 18:57:38 +0200

    Update global.d.ts

--- notes-manager-backend/src/global.d.ts
+++ notes-manager-backend/src/global.d.ts
@@ -2,6 +2,6 @@ import { Request } from 'express';
 import { IUser } from '../models/User';
 
 export interface AuthRequest extends Request {
-  user?: IUser;  // Додаємо властивість user
+  user?: IUser;  
 }
 
